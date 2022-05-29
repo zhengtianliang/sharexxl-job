ca6976792c506ab07a02dbf4087ca7e2b4fbc3fa
zhengtianliang <zhengtianliang156@163.com>
2022-05-29 22:54:42 +0800

    first commit

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot9997/src/main/java/com/xxl/job/executor/service/jobhandler/ShareSampleJob.java
@@ -0,0 +1,45 @@
+package com.xxl.job.executor.service.jobhandler;
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: ZhengTianLiang
+ * @date: 2022/05/27 17:13
+ * @desc:
+ */
+
+@Component
+public class ShareSampleJob {
+
+    private static Logger logger = LoggerFactory.getLogger(ShareSampleJob.class);
+
+    /**
+     * @author: ZhengTianLiang
+     * @date: 2022/05/27 17:13
+     * @desc: 测试1
+     */
+    @XxlJob("shareJobOneCopy")
+    public void test1() {
+        // 调度日志-操作-执行日志
+        XxlJobHelper.log("shareJobOneCopy copy start ... ");
+        // 控制台
+        logger.info("shareJobOneCopy copy test1 start ..");
+    }
+
+    /**
+     * @author: ZhengTianLiang
+     * @date: 2022/05/29 21:24
+     * @desc: 测试1
+     */
+    @XxlJob("shareJobOne")
+    public void test2() {
+        // 调度日志-操作-执行日志
+        XxlJobHelper.log("shareJobOne start ... ");
+        // 控制台
+        logger.info("shareJobOne test1 start ..");
+    }
+}
